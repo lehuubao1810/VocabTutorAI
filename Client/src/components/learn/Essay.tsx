dd182f0f962d2664dc8b72986468a0ec07cc298d
Minh Hiển phan <phanminhhien0701@gmail.com>
2024-05-19 14:16:32 +0700

    Resposive Learn part of web

--- Client/src/components/learn/Essay.tsx
+++ Client/src/components/learn/Essay.tsx
@@ -3,93 +3,99 @@ import { toast, ToastContainer } from "react-toastify";
 import { Vocabulary } from "../../type/Vocabulary";
 
 interface Props {
-  LearningData: Vocabulary[];
+	LearningData: Vocabulary[];
 }
 
 export const Essay: React.FC<Props> = ({ LearningData }) => {
-  const [currentVocabulary, setCurrentVocabulary] = useState<Vocabulary>(
-    LearningData[0]
-  );
-  const [currentVocabularyIndex, setCurrentVocabularyIndex] = useState(1);
-  const [userInput, setUserInput] = useState("");
-  const [IsHelp, setIsHelp] = useState(false);
+	const [currentVocabulary, setCurrentVocabulary] = useState<Vocabulary>(
+		LearningData[0]
+	);
+	const [currentVocabularyIndex, setCurrentVocabularyIndex] = useState(1);
+	const [userInput, setUserInput] = useState("");
+	const [IsHelp, setIsHelp] = useState(false);
 
-  useEffect(() => {
-    setCurrentVocabulary(LearningData[currentVocabularyIndex]);
-    setUserInput("");
-  }, [currentVocabularyIndex]);
+	useEffect(() => {
+		setCurrentVocabulary(LearningData[currentVocabularyIndex]);
+		setUserInput("");
+	}, [currentVocabularyIndex]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserInput(e.target.value);
-  };
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUserInput(e.target.value);
+	};
 
-  const handleCheckAnswer = () => {
-    if (
-      currentVocabulary &&
-      userInput.trim().toLowerCase() === currentVocabulary.word.toLowerCase()
-    ) {
-      toast.success("Correct!");
-      setTimeout(() => {
-        setCurrentVocabularyIndex(currentVocabularyIndex + 1);
-      }, 2000);
-    } else {
-      toast.error("Incorrect! Please try again.");
-    }
-  };
-  const HandleHelp = () => {
-    setIsHelp(true);
-  };
+	const handleCheckAnswer = () => {
+		if (
+			currentVocabulary &&
+			userInput.trim().toLowerCase() === currentVocabulary.word.toLowerCase()
+		) {
+			toast.success("Correct!");
+			setTimeout(() => {
+				setCurrentVocabularyIndex(currentVocabularyIndex + 1);
+			}, 2000);
+		} else {
+			toast.error("Incorrect! Please try again.");
+		}
+	};
+	const HandleHelp = () => {
+		setIsHelp(true);
+	};
 
-  return (
-    <>
-      {LearningData.length === 0 ? (
-        <div>
-          <h1>NO DATA TO LEARN</h1>
-        </div>
-      ) : (
-        <div className="mt-5 mb-10 h-full flex flex-col items-center">
-          <ToastContainer />
-          <div className="text-center mb-10">
-            <h1 className="text-3xl font-bold text-gray-600">
-              Learn by filling in words
-            </h1>
-            <p className="mt-2 text-xl font-semibold text-gray-500">
-              Please fill in the words according to the meaning you have learned
-            </p>
-          </div>
-          <div className="relative w-2/3 h-60 py-10 flex flex-col items-center gap-4 bg-white border-2 rounded-2xl">
-            <p className="text-lg font-semibold">
-              {currentVocabulary?.translation}
-            </p>
-            <p>{IsHelp ? currentVocabulary?.mean : ""}</p>
-            <div className=" flex gap-2">
-              <input
-                title="Answer"
-                type="text"
-                value={userInput}
-                onChange={handleInputChange}
-                className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              />
-              <button
-                type="button"
-                onClick={handleCheckAnswer}
-                className="px-5 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-              >
-                Check Answer
-              </button>
-            </div>
-            <div className="absolute top-2 right-2">
-              <button
-                type="button"
-                onClick={HandleHelp}
-                className="px-5 py-2 bg-blue-400 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-              >
-                Help ?
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{LearningData.length === 0 ? (
+				<div>
+					<h1>NO DATA TO LEARN</h1>
+				</div>
+			) : (
+				<div className="mt-5 mb-10 h-full flex flex-col items-center">
+					<ToastContainer />
+					<div className="text-center mb-10">
+						<h1 className="text-3xl font-bold text-gray-600">
+							Learn by filling in words
+						</h1>
+						<p className="mt-2 text-xl font-semibold text-gray-500">
+							Please fill in the words according to the meaning you have learned
+						</p>
+					</div>
+					<div
+						className="relative w-2/3 h-60 py-10 flex flex-col items-center gap-4 bg-white border-2 rounded-2xl
+                              max-sm:w-full max-sm:pt-14"
+					>
+						<p className="text-lg font-semibold">
+							{currentVocabulary?.translation}
+						</p>
+						<p>{IsHelp ? currentVocabulary?.mean : ""}</p>
+						<div
+							className="flex gap-2 
+                              max-sm:flex-col"
+						>
+							<input
+								title="Answer"
+								type="text"
+								value={userInput}
+								onChange={handleInputChange}
+								className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+							/>
+							<button
+								type="button"
+								onClick={handleCheckAnswer}
+								className="px-5 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+							>
+								Check Answer
+							</button>
+						</div>
+						<div className="absolute top-2 right-2">
+							<button
+								type="button"
+								onClick={HandleHelp}
+								className="px-5 py-2 bg-blue-400 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+							>
+								Help ?
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
--- Client/src/components/learn/Quizz.tsx
+++ Client/src/components/learn/Quizz.tsx
@@ -4,119 +4,128 @@ import "react-toastify/dist/ReactToastify.css";
 import { Vocabulary } from "../../type/Vocabulary";
 
 interface Props {
-  LearningData: Vocabulary[];
+	LearningData: Vocabulary[];
 }
 
 export const Quizz: React.FC<Props> = ({ LearningData }) => {
-  const [currentVocabulary, setCurrentVocabulary] = useState<Vocabulary>(
-    LearningData[0]
-  );
-  const [randomVocabularies, setRandomVocabularies] = useState<Vocabulary[]>(
-    []
-  );
-  const [correctAnswer, setCorrectAnswer] = useState<Vocabulary | null>(null);
-  const [isAnswered, setIsAnswered] = useState(false);
+	const [currentVocabulary, setCurrentVocabulary] = useState<Vocabulary>(
+		LearningData[0]
+	);
+	const [randomVocabularies, setRandomVocabularies] = useState<Vocabulary[]>(
+		[]
+	);
+	const [correctAnswer, setCorrectAnswer] = useState<Vocabulary | null>(null);
+	const [isAnswered, setIsAnswered] = useState(false);
 
-  const [currentVocabularyIndex, setCurrentVocabularyIndex] = useState(0);
+	const [currentVocabularyIndex, setCurrentVocabularyIndex] = useState(0);
 
-  console.log(isAnswered);
-  useEffect(() => {
-    setCurrentVocabulary(LearningData[currentVocabularyIndex]);
-    setIsAnswered(false);
-    const randomIndexes: number[] = [];
-    while (randomIndexes.length < 4) {
-      const randomIndex = Math.floor(Math.random() * LearningData.length);
-      if (!randomIndexes.includes(randomIndex)) {
-        randomIndexes.push(randomIndex);
-      }
-    }
-    const randomVocabularies = randomIndexes.map(
-      (index) => LearningData[index]
-    );
+	console.log(isAnswered);
+	useEffect(() => {
+		setCurrentVocabulary(LearningData[currentVocabularyIndex]);
+		setIsAnswered(false);
+		const randomIndexes: number[] = [];
+		while (randomIndexes.length < 4) {
+			const randomIndex = Math.floor(Math.random() * LearningData.length);
+			if (!randomIndexes.includes(randomIndex)) {
+				randomIndexes.push(randomIndex);
+			}
+		}
+		const randomVocabularies = randomIndexes.map(
+			(index) => LearningData[index]
+		);
 
-    // Lấy một từ làm đáp án đúng
-    const randomCorrectIndex = Math.floor(
-      Math.random() * randomVocabularies.length
-    );
-    const correctAnswer = randomVocabularies[randomCorrectIndex];
+		// Lấy một từ làm đáp án đúng
+		const randomCorrectIndex = Math.floor(
+			Math.random() * randomVocabularies.length
+		);
+		const correctAnswer = randomVocabularies[randomCorrectIndex];
 
-    setRandomVocabularies(randomVocabularies);
-    setCorrectAnswer(correctAnswer);
-    console.log("curentVocabulary", currentVocabulary);
-  }, [LearningData, currentVocabularyIndex, currentVocabulary]);
+		setRandomVocabularies(randomVocabularies);
+		setCorrectAnswer(correctAnswer);
+		console.log("curentVocabulary", currentVocabulary);
+	}, [LearningData, currentVocabularyIndex, currentVocabulary]);
 
-  const handleAnswerClick = (selectedVocabulary: Vocabulary) => {
-    // Kiểm tra xem từ đã chọn có phải là đáp án đúng không
-    if (selectedVocabulary === correctAnswer) {
-      setIsAnswered(true);
-      setTimeout(() => {
-        setCurrentVocabularyIndex(currentVocabularyIndex + 1);
-        if (currentVocabularyIndex + 1 >= LearningData.length) {
-          toast.success("Congratulations, you have finished the Quizz!");
-          // do something in here
-        }
-      }, 2000);
-      toast.success("Correct!");
-    } else {
-      setIsAnswered(false);
-      toast.error("Incorrect!");
-    }
-  };
+	const handleAnswerClick = (selectedVocabulary: Vocabulary) => {
+		// Kiểm tra xem từ đã chọn có phải là đáp án đúng không
+		if (selectedVocabulary === correctAnswer) {
+			setIsAnswered(true);
+			setTimeout(() => {
+				setCurrentVocabularyIndex(currentVocabularyIndex + 1);
+				if (currentVocabularyIndex + 1 >= LearningData.length) {
+					toast.success("Congratulations, you have finished the Quizz!");
+					// do something in here
+				}
+			}, 2000);
+			toast.success("Correct!");
+		} else {
+			setIsAnswered(false);
+			toast.error("Incorrect!");
+		}
+	};
 
-  // Kiểm tra nếu LearningData không phải là mảng hoặc rỗng, trả về một thông báo lỗi
-  if (!Array.isArray(LearningData) || LearningData.length === 0) {
-    return <div>No vocabulary data available</div>;
-  }
+	// Kiểm tra nếu LearningData không phải là mảng hoặc rỗng, trả về một thông báo lỗi
+	if (!Array.isArray(LearningData) || LearningData.length === 0) {
+		return <div>No vocabulary data available</div>;
+	}
 
-  return (
-    <>
-      {LearningData.length === 0 ? (
-        <div>
-          <h1>NO DATA TO LEARN</h1>
-        </div>
-      ) : (
-        <div className="mt-5 mb-10 h-full flex flex-col items-center">
-          <ToastContainer />
-          <div className="text-center mb-10">
-            <h1 className="text-3xl font-bold text-gray-600">
-              Study with Quizzes
-            </h1>
-            <p className="mt-2 text-xl font-semibold text-gray-500">
-              Choose the correct answer to the question below.
-            </p>
-          </div>
-          <div className="relative w-2/3 h-44 py-10 flex flex-col items-center gap-4 bg-white border-2 rounded-2xl">
-            {correctAnswer && (
-              <div className="flex flex-col items-center gap-4">
-                <div className="absolute top-2 left-3 w-full h-full">
-                  <p className="w-10 h-10 flex items-center justify-center rounded-full border-2 bg-slate-50 text-xl font-bold text-gray-500">
-                    {currentVocabularyIndex + 1}
-                  </p>
-                </div>
-                <p className="text-2xl font-semibold">{correctAnswer.word}</p>
-                <p>{correctAnswer.pronunciation}</p>
-              </div>
-            )}
-          </div>
-          <div className="my-10 grid grid-cols-2 gap-4 w-2/3">
-            {randomVocabularies.map((vocabulary, index) => (
-              <button
-                key={index}
-                className={`px-5 py-3 border-2 rounded-xl ${
-                  isAnswered && vocabulary === correctAnswer
-                    ? "bg-green-200"
-                    : isAnswered && vocabulary !== correctAnswer
-                    ? "bg-red-200"
-                    : "bg-white"
-                }`}
-                onClick={() => handleAnswerClick(vocabulary)}
-              >
-                {vocabulary.translation}
-              </button>
-            ))}
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{LearningData.length === 0 ? (
+				<div>
+					<h1>NO DATA TO LEARN</h1>
+				</div>
+			) : (
+				<div
+					className="mt-5 mb-10 h-full flex flex-col items-center
+                          "
+				>
+					<ToastContainer />
+					<div className="text-center mb-10">
+						<h1 className="text-3xl font-bold text-gray-600">
+							Study with Quizzes
+						</h1>
+						<p className="mt-2 text-xl font-semibold text-gray-500">
+							Choose the correct answer to the question below.
+						</p>
+					</div>
+					<div
+						className="relative w-2/3 h-44 py-10 flex flex-col items-center gap-4 bg-white border-2 rounded-2xl
+                               max-sm:w-full max-sm:pt-14"
+					>
+						{correctAnswer && (
+							<div className="flex flex-col items-center gap-4">
+								<div className="absolute top-2 left-3 w-full h-full">
+									<p className="w-10 h-10 flex items-center justify-center rounded-full border-2 bg-slate-50 text-xl font-bold text-gray-500">
+										{currentVocabularyIndex + 1}
+									</p>
+								</div>
+								<p className="text-2xl font-semibold">{correctAnswer.word}</p>
+								<p>{correctAnswer.pronunciation}</p>
+							</div>
+						)}
+					</div>
+					<div
+						className="my-10 grid grid-cols-2 gap-4 w-2/3
+                            max-sm:grid-cols-1 max-sm:w-full max-sm:gap-1 max-sm:my-5"
+					>
+						{randomVocabularies.map((vocabulary, index) => (
+							<button
+								key={index}
+								className={`px-5 py-3 border-2 rounded-xl ${
+									isAnswered && vocabulary === correctAnswer
+										? "bg-green-200"
+										: isAnswered && vocabulary !== correctAnswer
+										? "bg-red-200"
+										: "bg-white"
+								}`}
+								onClick={() => handleAnswerClick(vocabulary)}
+							>
+								{vocabulary.translation}
+							</button>
+						))}
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
