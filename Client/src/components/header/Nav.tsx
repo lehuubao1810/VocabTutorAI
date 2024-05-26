e044953ff929ee95e810d0e307587567640aaf5f
Minh Hiển phan <phanminhhien0701@gmail.com>
2024-05-26 16:11:43 +0700

    Adjust Header and responsive

--- Client/src/components/header/Nav.tsx
+++ Client/src/components/header/Nav.tsx
@@ -1,41 +1,45 @@
 import { NavLink } from "react-router-dom";
 
 type Props = {
-  // type of the props
+	// type of the props
 };
 
 export const Nav: React.FC<Props> = (props) => {
-  console.log(props);
-  const navLink = [
-    {
-      to: "/",
-      text: "Home",
-    },
-    {
-      to: "/characters-ai",
-      text: "Characters AI",
-    },
-  ];
-  return (
-    <ul className="flex gap-4 items-center">
-      {navLink.map((link, index) => {
-        return (
-          <li key={index}>
-            <NavLink
-              className={({ isActive }) => `
+	console.log(props);
+	const navLink = [
+		{
+			to: "/",
+			text: "Home",
+		},
+		{
+			to: "/characters-ai",
+			text: "Characters AI",
+		},
+		{
+			to: "/explore",
+			text: "Explore",
+		},
+	];
+	return (
+		<ul className="flex gap-4 items-center lg:gap-8">
+			{navLink.map((link, index) => {
+				return (
+					<li key={index}>
+						<NavLink
+							className={({ isActive }) => `
                 text-base font-semibold ${
-                  isActive
-                    ? "text-blue-500 border-blue-500 border-b-4 pb-2 rounded-bl rounded-br"
-                    : "text-gray-500"
-                }
+									isActive
+										? "text-blue-500 border-blue-500 border-b-4 pb-2 rounded-bl rounded-br"
+										: "text-gray-500"
+								}
                 `}
-              to={link.to}
-            >
-              {link.text}
-            </NavLink>
-          </li>
-        );
-      })}
-    </ul>
-  );
+							to={link.to}
+						>
+							{link.text}
+						</NavLink>
+					</li>
+				);
+			})}
+		</ul>
+	);
 };
