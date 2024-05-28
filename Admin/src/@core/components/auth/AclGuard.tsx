5644924f6369cf538ef9d035d3d8e82eecb83fac
lehuubao1810 <lehuubao2909@gmail.com>
2024-05-28 14:17:59 +0700

    fix: Update AclGuard to fix buildAbilityFor function call

--- Admin/src/@core/components/auth/AclGuard.tsx
+++ Admin/src/@core/components/auth/AclGuard.tsx
@@ -51,7 +51,8 @@ const AclGuard = (props: AclGuardProps) => {
 
   // // User is logged in, build ability for the user based on his role
   if (auth.user && !ability) {
-    ability = buildAbilityFor(aclAbilities.subject)
+    // ability = buildAbilityFor(aclAbilities.subject)
+    ability = buildAbilityFor()
     if (router.route === '/') {
       return <Spinner />
     }
