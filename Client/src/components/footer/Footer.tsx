70203930a472eb95791a6ec4d87ac6e0c7030968
Minh Hiển phan <phanminhhien0701@gmail.com>
2024-05-30 12:20:40 +0700

    Reedit Footer

--- Client/src/components/footer/Footer.tsx
+++ Client/src/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import {
 	faFacebook,
 	faInstagram,
+	faLinkedin,
 	faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type FooterProps = {
 	//
@@ -35,19 +37,14 @@ const Footer: React.FC<FooterProps> = () => {
 	const navigate = useNavigate();
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log("submit");
+		toast.success("Thanks for your interest in TutorAI!");
 	};
 
 	const homeLinks = [
 		{ to: "/", label: "Home" },
-		{ to: "/learn", label: "Learn" },
-		{ to: "/about", label: "Character AI" },
-		{ to: "/about", label: "About" },
-	];
-
-	const collectionLinks = [
 		{ to: "#!", label: "Collection" },
-		{ to: "#!", label: "Learn" },
+		{ to: "#!", label: "Learn Quizz" },
+		{ to: "#!", label: "Learn Essay" },
 	];
 
 	const characterAILinks = [
@@ -55,9 +52,11 @@ const Footer: React.FC<FooterProps> = () => {
 		{ to: "#!", label: "Room AI" },
 	];
 
+	const collectionLinks = [{ to: "/explore", label: "Explore" }];
+
 	return (
 		<footer className="bg-gray-800 text-white">
-			<div className=" ml-auto mr-auto max-w-screen-xl px-5 py-20 max-md:py-10">
+			<div className=" ml-auto mr-auto max-w-screen-xl px-5 pt-20 pb-5 max-md:pt-10 max-md:pb-3">
 				<div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-10">
 					<div className="flex flex-col gap-4">
 						{/* Logo */}
@@ -88,13 +87,16 @@ const Footer: React.FC<FooterProps> = () => {
 					</div>
 					{/* Link trang web */}
 					<FooterList title="Home page" items={homeLinks} />
-					<FooterList title="Collection" items={collectionLinks} />
 					<FooterList title="Character AI" items={characterAILinks} />
+					<FooterList title="Explore" items={collectionLinks} />
 					{/* Social media */}
 					<div className="flex flex-col gap-4">
 						<h3 className="text-xl">Social Media</h3>
 						<div className="flex gap-4">
-							<a href="!#" className="text-white hover:text-blue-500">
+							<a
+								href="https://www.facebook.com/bb.huubao"
+								className="text-white hover:text-blue-500"
+							>
 								<FontAwesomeIcon icon={faFacebook} />
 							</a>
 							<a href="!#" className="text-white hover:text-blue-500">
@@ -103,8 +105,11 @@ const Footer: React.FC<FooterProps> = () => {
 							<a href="!#" className="text-white hover:text-blue-500">
 								<FontAwesomeIcon icon={faTwitter} />
 							</a>
-							<a href="!#" className="text-white hover:text-blue-500">
-								<FontAwesomeIcon icon={faInstagram} />
+							<a
+								href="https://www.linkedin.com/in/minh-hi%E1%BB%83n-phan-30007b216/"
+								className="text-white hover:text-blue-500"
+							>
+								<FontAwesomeIcon icon={faLinkedin} />
 							</a>
 						</div>
 					</div>
@@ -112,7 +117,8 @@ const Footer: React.FC<FooterProps> = () => {
 				{/* Copyright */}
 				<div className="mt-8 text-center">
 					<p>
-						&copy; {new Date().getFullYear()} VocabTutor. All rights reserved.
+						© {new Date().getFullYear()} Vocab Tutor. All rights will be
+						registered
 					</p>
 				</div>
 			</div>
